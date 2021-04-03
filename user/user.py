020611d08924bdf5d3de863356f14e9cf4ef3ba5
makennamartin97 <makennamartin97@yahoo.com>
2021-04-03 17:38:04 -0400

    User assignment

--- user/user.py
+++ user/user.py
@@ -9,13 +9,17 @@ class User:
     # Add a make_withdrawal method to the User class
     def make_withdrawal(self, amount):
         self.balance -= amount
+        return self
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
     
     def transfer_money(self, otheruser, amount):
+        print(f"{self.name} is transferring {amount}$ to {otheruser.name}")
         otheruser.make_deposit(amount)
         self.make_withdrawal(amount)
+        return self
 
 
 
@@ -26,27 +30,15 @@ user3 = User('Brian')
 
 # Have the first user make 3 deposits and 1 withdrawal and 
 # then display their balance
-user1.make_deposit(300)
-user1.make_deposit(90)
-user1.make_deposit(20)
-user1.make_withdrawal(5)
-user1.display_balance()
+user1.make_deposit(300).make_deposit(90).make_deposit(20).make_withdrawal(5).display_balance()
 
 # Have the second user make 2 deposits and 2 withdrawals and 
 # then display their balance
-user2.make_withdrawal(-1000)
-user2.make_deposit(365)
-user2.make_deposit(5)
-user2.make_withdrawal(-12)
-user2.display_balance()
+user2.make_withdrawal(-1000).make_deposit(365).make_deposit(5).make_withdrawal(-12).display_balance()
 
 # Have the third user make 1 deposits and 3 withdrawals and 
 # then display their balance
-user3.make_deposit(20)
-user3.make_withdrawal(1)
-user3.make_withdrawal(400)
-user3.make_withdrawal(26)
-user3.display_balance()
+user3.make_deposit(20).make_withdrawal(1).make_withdrawal(400).make_withdrawal(26).display_balance()
 
 # BONUS: Add a transfer_money method; have the first user 
 # transfer money to the third user and then print both users' balances
